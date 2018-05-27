44f64b0eec43689ffae213aa245caa25efe69e2a
Alex Zhang <alexgzhang92@gmail.com>
2018-05-27 05:24:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(array)
   if array.size == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     sentence = "The line is currently:"
     count = 1
@@ -13,3 +13,25 @@ def line(array)
     return sentence
   end
 end
+
+def take_a_number(array, name)
+  if array.size == 0
+    puts "Welcome, #{name}. You are number 1 in line."
+    array.push(name)
+  else
+    puts "Welcome, #{name}. You are number #{array.size+1} in line."
+    array.push(name)
+  end
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.unshift
+  end
+end
+
+
+
