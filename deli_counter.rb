fab453ec3170ed4da8b90757bbc5ca07e49213ff
Alex Zhang <alexgzhang92@gmail.com>
2018-05-27 05:10:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,10 @@ def line(array)
   if array.size == 0
     return "The line is currently empty."
   else
-    
+    sentence = ["The line is currently: "]
+    count = 1
+    array.each do |name|
+      
+    end
+  end
 end
