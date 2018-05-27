ec619d6f020d3da5962cc9c3ea1935ace3d78dfc
Alex Zhang <alexgzhang92@gmail.com>
2018-05-27 05:17:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,12 @@ def line(array)
   if array.size == 0
     return "The line is currently empty."
   else
-    sentence = "The line is currently: "
+    sentence = "The line is currently:"
     count = 1
     array.each do |name|
-      setence.push(count.to_s)
-      sentence.push(name)
+      setence.push(" #{count}. #{name}")
+      count+=1
     end
+    return sentence
   end
 end
