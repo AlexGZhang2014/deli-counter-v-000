105bc01cf1eefa5b5ba26576a8be0a1d1c3e0628
Alex Zhang <alexgzhang92@gmail.com>
2018-05-27 05:27:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
   else
     sentence = "The line is currently:"
     array.each_with_index do |name, index|
-      setence.push(" #{index+1}. #{name}")
+      sentence.push(" #{index+1}. #{name}")
     end
     return sentence
   end
@@ -27,7 +27,7 @@ def now_serving(array)
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{array[0]}."
-    array.unshift
+    array.unshift!
   end
 end
 
